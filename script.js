fdf6b4a2244740397fa91249cdd9f089509d8739
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-23 14:30:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
+const button = document.querySelector(".btn");
+
+button.addEventListener('click',function (e) {
+   search.classList.toggle('active');
+   input.classList.toggle('active');    
+   input.focus(); 
+});
