3f9662160bdefa011390832134c1295eba3bb742
Gulamnabi Kagathala <145833927+gulu43@users.noreply.github.com>
2025-03-09 05:44:28 -0300

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //your JS code here. If required.
-const search = document.querySelector(".search");
-const input = document.querySelector(".input");
-const button = document.querySelector(".btn");
+const searchDiv = document.querySelector(".search");
+const inputField = document.querySelector(".input");
+const searchBtn = document.querySelector(".btn");
 
-button.addEventListener('click',function (e) {
-   search.classList.toggle('active');
-   input.classList.toggle('active');    
-   input.focus(); 
+searchBtn.addEventListener("click", () => {
+    searchDiv.classList.add("active");
+    inputField.focus();
 });
